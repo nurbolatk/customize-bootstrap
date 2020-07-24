e12be859674c60f8104ffe918fc5be8e6373e5e0
nurbolatk <nurbolattk@gmail.com>
2020-07-24 14:45:52 +0600

    Create apply promocode form's functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "./node_modules/bootstrap/dist/js/bootstrap.js";
 var nextPlaceOrderBtn = document.getElementById("next-place-order-btn");
+const promocodeForm = document.getElementById("promocode-form");
 
 // Listen for tabs click
 var tabEls = document.querySelectorAll('a[data-toggle="pill"]');
@@ -29,4 +30,60 @@ nextPlaceOrderBtn.addEventListener("click", function (e) {
     e.target.dataset.tabIndex = currentTabIndex + 1;
   }
   tab.show();
+});
+
+function createPromocodeFeedback(isValid) {
+  if (isValid) {
+    return `
+    <div style="display: block;" class="valid-feedback">
+    Promocode applied
+    </div>
+    `;
+  } else {
+    return `
+    <div style="display: block;" class="invalid-feedback">
+    No such promocode found
+    </div>
+    `;
+  }
+}
+
+// Event listener for promocode submit
+promocodeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const enteredPromocode = promocodeForm[0].value.toUpperCase();
+  if (enteredPromocode.length) {
+    if (enteredPromocode === "EASYSHOP20") {
+      // correct
+      // check if feedback already exists
+      const oldFeedback = promocodeForm.querySelector('[class$="-feedback"');
+      console.log(oldFeedback);
+      if (oldFeedback) {
+        // replace if exists
+        oldFeedback.remove();
+      }
+      // else append
+      promocodeForm
+        .querySelector(".input-group")
+        .insertAdjacentHTML("beforeend", createPromocodeFeedback(true));
+      // add styles to input
+      promocodeForm[0].classList.remove("is-invalid");
+      promocodeForm[0].classList.add("is-valid");
+    } else {
+      // not correct
+      // check if feedback already exists
+      const oldFeedback = promocodeForm.querySelector('[class$="-feedback"');
+      console.log(oldFeedback);
+      if (oldFeedback) {
+        // replace if exists
+        oldFeedback.remove();
+      }
+      // else append
+      promocodeForm
+        .querySelector(".input-group")
+        .insertAdjacentHTML("beforeend", createPromocodeFeedback(false));
+      promocodeForm[0].classList.remove("is-valid");
+      promocodeForm[0].classList.add("is-invalid");
+    }
+  }
 });
