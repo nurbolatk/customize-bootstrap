f6eea0b0d10eec9e3c3cdddcc31c194f9fc64216
nurbolatk <nurbolattk@gmail.com>
2020-07-26 00:53:15 +0600

    Install popper.js, remove node-sass

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import "./node_modules/bootstrap/dist/js/bootstrap.js";
+// import "./node_modules/bootstrap/dist/js/bootstrap.js";
+import "bootstrap";
+import "./sass/custom.scss"; // Import our scss file
 var nextPlaceOrderBtn = document.getElementById("next-place-order-btn");
 const promocodeForm = document.getElementById("promocode-form");
 
